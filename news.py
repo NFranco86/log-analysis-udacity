fe153758381e5cab648b9b09046f0569cb573239
Mike Mulligan <mkmllgn@gmail.com>
2017-07-17 14:02:39 -0400

    Update python file to meet pep8 standards

--- news.py
+++ news.py
@@ -3,9 +3,9 @@ import psycopg2
 
 database_name = "news"
 
-#Store queries into variables
+# Store queries into variables
 q1 = ("""SELECT title, count(*) as num FROM articles, log
-      WHERE articles.slug = substring(log.path, 10) 
+      WHERE articles.slug = substring(log.path, 10)
       GROUP BY title ORDER BY num DESC LIMIT 3;""")
 q2 = ("""SELECT authors.name, count(*) AS num
       FROM articles
@@ -18,15 +18,18 @@ q3 = ("""SELECT to_char(errors.date, 'Mon DD, YYYY'),
 
 # Store results
 q1_results = dict()
-q1_results['title'] = "1. The most popular three articles of all time?"
+q1_results['title'] = (
+    "1. The most popular three articles of all time?")
 q1_results['ending'] = " views"
 
 q2_results = dict()
-q2_results['title'] = "2. Who are the most popular article authors of all time?"
+q2_results['title'] = (
+    "2. Who are the most popular article authors of all time?")
 q2_results['ending'] = " views"
 
 q3_results = dict()
-q3_results['title'] = "3. On which days did more than 1% of requests lead to errors?"
+q3_results['title'] = (
+    "3. On which days did more than 1% of requests lead to errors?")
 q3_results['ending'] = "%"
 
 
@@ -39,11 +42,14 @@ def get_results(query):
     db.close()
     return results
 
+
 # Print results method
 def print_query_results(query_results):
     print (query_results['title'])
     for result in query_results['results']:
-        print ('\t' + str(result[0]) + ': ' + str(result[1]) + query_results['ending'])
+        print ('\t' + str(result[0]) + ': ' +
+               str(result[1]) +
+               query_results['ending'])
 
 # Store query results
 q1_results['results'] = get_results(q1)
@@ -54,6 +60,3 @@ q3_results['results'] = get_results(q3)
 print_query_results(q1_results)
 print_query_results(q2_results)
 print_query_results(q3_results)
-    
-
-
